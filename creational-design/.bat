fd9a6ad4ed6a89ee76a54ae5532b47b801406284
piyachai kaewchum <kaonaka@hotmail.co.th>
2022-11-01 21:18:48 +0700

    :hammer: add [creational-design]

---
+++ creational-design/.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+cmd /C ""C:\Program Files\Java\jdk-16.0.1\bin\java.exe" -XX:+ShowCodeDetailsInExceptionMessages @C:\Users\Lenovo\AppData\Local\Temp\cp_e3awwlgy8bye0zbd8stcolgp1.argfile creational.Main " > output.txt
+PAUSE
