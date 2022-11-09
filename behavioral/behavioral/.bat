a94476915dfc3a49b7017e893ffca6c3de6f22fd
piyachai kaewchum <kaonaka@hotmail.co.th>
2022-11-09 22:54:03 +0700

    :sparkles: add behavioral lab

---
+++ behavioral/behavioral/.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+cmd /C ""C:\Program Files\Java\jdk-16.0.1\bin\java.exe" -XX:+ShowCodeDetailsInExceptionMessages @C:\Users\Lenovo\AppData\Local\Temp\cp_7g4fda87wsz09dzko1nkm7yd3.argfile behavioral.Main " > output.txt
+PAUSE
